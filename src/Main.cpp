0f1bc2e261deeaf8f941e88ade0cfc5e4402a39d
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-04-24 14:56:30 +0200

    feat(core): add project compilation system

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** OOP_raytracer_2023 [WSL: Ubuntu]
+** File description:
+** Main
+*/
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    return 0;
+}
