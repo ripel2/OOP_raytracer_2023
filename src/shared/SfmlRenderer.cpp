803c05171f727aa3c111ed7ccff817e2fa2f24d4
Guyony <thibault.guyony@epitech.eu>
2023-05-08 16:10:30 +0200

    refactor(sfml): display with close event

--- src/shared/SfmlRenderer.cpp
+++ src/shared/SfmlRenderer.cpp
@@ -27,18 +27,15 @@ void RayTracer::SfmlRenderer::render(std::size_t width, std::size_t height, cons
     std::size_t index = 0;
     sf::Event event;
     size_t size = 1;
-
-    for (std::size_t i = 0; i < height; i++) {
-        for (std::size_t j = 0; j < width; j++) {
-            Color color = getColor((double)j / (double)width, 1.0 - ((double)i / (double)height), scene);
-            array[index].position = sf::Vector2f(j, i);
-            array[index].color = sf::Color(color[0] * 255, color[1] * 255, color[2] * 255);
-            window.draw(&array[index], size, sf::Points);
-        }
-    }
-    window.display();
     while (event.type != sf::Event::Closed) {
-        if (event.type == sf::Event::Closed)
-            window.close();
+        for (std::size_t i = 0; i < height; i++) {
+            for (std::size_t j = 0; j < width; j++) {
+                Color color = getColor((double)j / (double)width, 1.0 - ((double)i / (double)height), scene);
+                array[index].position = sf::Vector2f(j, i);
+                array[index].color = sf::Color(color[0] * 255, color[1] * 255, color[2] * 255);
+                window.draw(&array[index], size, sf::Points);
+            }
+        }
+        window.display();
     }
 }
