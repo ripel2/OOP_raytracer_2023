3df24cbf3225b111085d45e9610e13bf296f9af1
foxaxe <axel.idoux@epitech.eu>
2023-05-10 21:03:02 +0200

    feat(PPMrenderer): add multithreading to the renderer to render faster

--- src/main/PPMRenderer.cpp
+++ src/main/PPMRenderer.cpp
@@ -10,27 +10,55 @@
 #include <fstream>
 
 RayTracer::PPMRenderer::PPMRenderer(const std::string &filename)
-    : filename(filename)
+    : _filename(filename), _linesDone(0), _mutex()
 {
+}
+
+void *RayTracer::PPMRenderer::execRenderThread(std::size_t width, std::size_t height, const Scene &scene, std::size_t start, std::size_t end)
+{
+    for (std::size_t i = start; i < end; i++) {
+        for (std::size_t j = 0; j < width; j++) {
+            Color color = getColor(static_cast<double>(j) / static_cast<double>(width), 1.0 - (static_cast<double>(i) / static_cast<double>(height)), scene);
+            _image[i].push_back(color);
+        }
+        _mutex.lock();
+        _linesDone++;
+        std::cout << "\r" << _linesDone << "/" << height << " lines done";
+        _mutex.unlock();
+    }
+    return nullptr;
 }
 
 void RayTracer::PPMRenderer::render(std::size_t width, std::size_t height, const Scene &scene)
 {
-    std::fstream file(filename, std::ios::out);
+    std::fstream file(_filename, std::ios::out);
+    std::vector<std::thread> threads;
+    std::size_t thread_nb = 1;
+    std::size_t linesPerThread;
 
+    for (std::size_t i = 1; i <= std::thread::hardware_concurrency(); i++) {
+        if (height % i == 0) {
+            thread_nb = i;
+        }
+    }
+    linesPerThread = height / thread_nb;
     if (!file.is_open())
         throw std::runtime_error("Failed to open file");
+    _image.resize(height);
+    for (std::size_t i = 0; i < thread_nb; i++) {
+        threads.emplace_back(&PPMRenderer::execRenderThread, this, width, height, std::ref(scene), i * linesPerThread, (i + 1) * linesPerThread);
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
+    std::cout << std::endl;
     file << "P3" << std::endl;
     file << width << " " << height << " " << 255 << std::endl;
-    for (std::size_t i = 0; i < height; i++) {
-        std::cout << "\rRendering: " << (int)(((double)i / (double)height) * 100) << "%" << std::flush;
-        for (std::size_t j = 0; j < width; j++) {
-            Color color = getColor((double)j / (double)width, 1.0 - ((double)i / (double)height), scene);
-
+    for (std::vector<Color> &line : _image) {
+        for (Color &color : line) {
             file << (int)(color[0] * 255) << " " << (int)(color[1] * 255) << " " << (int)(color[2] * 255) << " ";
         }
         file << std::endl;
     }
     file.close();
-    std::cout << "\rRendering: 100%" << std::endl;
 }
