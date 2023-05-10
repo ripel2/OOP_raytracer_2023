d8c93957f787cc156d97f56801925a3c451100ad
Axel <91660228+FoxaxeWasTaken@users.noreply.github.com>
2023-05-10 22:57:16 +0200

    feat(core): merge branch multithreading (#77)
    * feat(PPMrenderer): add multithreading to the renderer to render faster
    
    * refactor(pointlight): turn the rand into rand_r to be thread safe

--- src/main/PPMRenderer.hpp
+++ src/main/PPMRenderer.hpp
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include "thread"
+#include "mutex"
+
 #include "ARenderer.hpp"
 
 namespace RayTracer {
@@ -32,9 +35,19 @@ namespace RayTracer {
             void render(std::size_t width, std::size_t height, const Scene &scene) override;
 
         private:
+            std::string _filename;
+            std::vector<std::vector<Color>> _image;
+            std::size_t _linesDone;
+            std::mutex _mutex;
             /**
-             * @brief The filename of the image
+             * @brief The function executed by each thread
+             * @param width The width of the image
+             * @param height The height of the image
+             * @param scene The scene to render
+             * @param start The start of the lines to render
+             * @param end The end of the lines to render
+             * @return nullptr
              */
-            std::string filename;
+            void *execRenderThread(std::size_t width, std::size_t height, const Scene &scene, std::size_t start, std::size_t end);
     };
 }
